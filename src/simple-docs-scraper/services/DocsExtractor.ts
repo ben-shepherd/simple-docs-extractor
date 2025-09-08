0681b179ed9c6d98047e5f3686bdc4ab5306fcc0
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-09-08 18:32:45 +0100

    Refactor DocsExtractor to support multiple extraction methods (tags, regex, callback) and update tests accordingly. Enhance error handling for misconfigured extraction methods.

--- src/simple-docs-scraper/services/DocsExtractor.ts
+++ src/simple-docs-scraper/services/DocsExtractor.ts
@@ -1,28 +1,39 @@
 import fs from 'fs';
 
-export type DocsExtractorConfig = {
-    tags: {
-        startDocs: string;
-        endDocs: string;
-    };
+export type MethodTags = {
+    extractMethod: 'tags';
+    startTag: string;
+    endTag: string;
 }
+
+export type MethodRegex = {
+    extractMethod: 'regex';
+    pattern: RegExp;
+}
+
+export type MethodCallback = {
+    extractMethod: 'callback';
+    callback: (fileContent: string) => Promise<string | undefined> | (string | undefined);
+}
+
 
 export type DocsExtractorResult = {
     docs: string;
     file: string;
     sucess?: boolean;
-    errorType?: 'fileNotFound' | 'noStartOrEndTags' | 'noDocs';
+    errorType?: 'fileNotFound' | 'noStartOrEndTags' | 'noDocs' | 'misconfigured';
+    errorMessage?: string;
 }
 
 export class DocsExtractor {
 
     constructor(
         private file: string,
-        private config: DocsExtractorConfig
+        private config: MethodTags | MethodRegex | MethodCallback
     ) {
     }
 
-    extract(): DocsExtractorResult {
+    async extract(): Promise<DocsExtractorResult> {
 
         // Check if the file exists
         if (!fs.existsSync(this.file)) {
@@ -35,9 +46,87 @@ export class DocsExtractor {
         }
 
         const fileContent = fs.readFileSync(this.file, 'utf8');
+        let result: DocsExtractorResult;
+
+        if(this.config.extractMethod === 'tags' && this.config.startTag && this.config.endTag) {
+            result = this.extractUsingTags(fileContent);
+        }
+        else if(this.config.extractMethod === 'regex' && this.config.pattern) {
+            result = this.extractUsingRegex(fileContent);
+        }
+        else if(this.config.extractMethod === 'callback' && typeof this.config.callback === 'function') {
+            result = await this.extractUsingCallback(fileContent);
+        }
+        else {
+            throw new Error('You must provide a valid extract method');
+        }
+
+        if(!result.sucess) {
+            return result;
+        }
+
+        // trim spaces and empty lines
+        result.docs = result.docs.trim().replace(/\n\s*\n/g, '\n');
+
+        return {
+            sucess: true,
+            docs: result.docs,
+            file: this.file
+        };
+    }
+
+
+    protected extractUsingRegex(fileContent: string): DocsExtractorResult {
+        const config = this.config as MethodRegex;
+
+        const regex = new RegExp(config.pattern);
+
+        const match = fileContent.match(regex);
+
+        if (!match) {
+            return {
+                sucess: false,
+                docs: '',
+                file: this.file,
+                errorType: 'noDocs',
+            };
+        }
+
+        const docs = match[1];
+
+        return {
+            sucess: true,
+            docs: docs,
+            file: this.file
+        };
+    }
+
+    protected async extractUsingCallback(fileContent: string): Promise<DocsExtractorResult> {
+        const config = this.config as MethodCallback;
+
+        const docs = await config.callback(fileContent);
+
+        if(!docs) {
+            return {
+                sucess: false,
+                docs: '',
+                file: this.file,
+                errorType: 'noDocs',
+            };
+        }
+
+        return {
+            sucess: true,
+            docs: docs,
+            file: this.file
+        };
+    }
+
+    protected extractUsingTags(fileContent: string): DocsExtractorResult {
+        const config = this.config as MethodTags;
 
         // Check if the file contains the start and end tags
-        if (!fileContent.includes(this.config.tags.startDocs) || !fileContent.includes(this.config.tags.endDocs)) {
+        if (!fileContent.includes(config.startTag) || !fileContent.includes(config.endTag)) {
             return {
                 sucess: false,
                 docs: '',
@@ -46,16 +135,13 @@ export class DocsExtractor {
             };
         }
 
-        const startIndex = fileContent.indexOf(this.config.tags.startDocs);
-        const endIndex = fileContent.indexOf(this.config.tags.endDocs);
+        const startIndex = fileContent.indexOf(config.startTag);
+        const endIndex = fileContent.indexOf(config.endTag);
         let docs = fileContent.substring(startIndex, endIndex);
 
         // strip the start and end tags
-        docs = docs.replace(this.config.tags.startDocs, '')
-        docs = docs.replace(this.config.tags.endDocs, '');
-
-        // trim spaces and empty lines
-        docs = docs.trim().replace(/\n\s*\n/g, '\n');
+        docs = docs.replace(config.startTag, '')
+        docs = docs.replace(config.endTag, '');
 
         return {
             sucess: true,
--- src/tests/DocsExtractor.test.ts
+++ src/tests/DocsExtractor.test.ts
@@ -1,63 +1,141 @@
-import { beforeEach, describe, expect, test } from "@jest/globals";
+import { describe, expect, test } from "@jest/globals";
 import { DocsExtractor } from "../simple-docs-scraper/services/DocsExtractor.js";
 
 describe("Docs Extractor", () => {
     let docsExtractor!: DocsExtractor;
-    let files!: string[];
+    const fileWithDocs = process.cwd() + '/src/tests/js-files/exampleFunc.js';
+    const fileWithoutDocs = process.cwd() + '/src/tests/js-files/exampleFuncNoDocs.js';
 
-    beforeEach(() => {
-        files = [
-            process.cwd() + '/src/tests/js-files/exampleFunc.js',
-            process.cwd() + '/src/tests/js-files/exampleTsFunc.ts',
-            process.cwd() + '/src/tests/js-files/nested-js-files/nestedFunc.js',
-            process.cwd() + '/src/tests/js-files/exampleFuncNoDocs.js',
-        ];
-    });
+    describe("config", () => {
+        test("should throw an error if the extract method is not valid", async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'invalid',
+            } as any);
 
-    describe("extract", () => {
-        test("should perform expected behavior", () => {
-            docsExtractor = new DocsExtractor(files[0], {
-                tags: {
-                    startDocs: '<docs>',
-                    endDocs: '</docs>',
-                }
+            await expect(docsExtractor.extract()).rejects.toThrow('You must provide a valid extract method');
+        })
+
+        test('should accept a valid extract tags method', async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'tags',
+                startTag: '<docs>',
+                endTag: '</docs>',
             });
 
-            const result = docsExtractor.extract();
+            await expect(docsExtractor.extract()).resolves.not.toThrow();
+        })
 
-            console.log(1, result.docs);
+        test('should accept regex extract method', async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'regex',
+                pattern: new RegExp('/<docs>(.*?)<\/docs>/s'),
+            });
+            
+            await expect(docsExtractor.extract()).resolves.not.toThrow();
+        })
+
+        test('should accept callback extract method', async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'callback',
+                callback: async (fileContent) => {
+                    return fileContent.match('/<docs>(.*?)<\/docs>/s')?.[1];
+                },
+            });
+            
+            await expect(docsExtractor.extract()).resolves.not.toThrow();
+        })
+    });
+
+    describe("extract using tags", () => {
+        test("should perform expected behavior", async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'tags',
+                startTag: '<docs>',
+                endTag: '</docs>',
+            });
+
+            const result = await docsExtractor.extract();
 
             expect(result.sucess).toBe(true);
             expect(result.docs).toContain('#exampleFunc.js');
         })
 
-        test("should return an error if the file does not contain the start and end tags", () => {
-            docsExtractor = new DocsExtractor(files[3], {
-                tags: {
-                    startDocs: '<docs>',
-                    endDocs: '</docs>',
-                }
+        test("should return an error if the file does not contain the start and end tags", async () => {
+            docsExtractor = new DocsExtractor(fileWithoutDocs, {
+                extractMethod: 'tags',
+                startTag: '<docs>',
+                endTag: '</docs>',
             });
 
-            const result = docsExtractor.extract();
+            const result = await docsExtractor.extract();
 
             expect(result.sucess).toBe(false);
             expect(result.errorType).toBe('noStartOrEndTags');
         })
 
 
-        test("should return an error if the file does not exist", () => {
+        test("should return an error if the file does not exist", async () => {
             docsExtractor = new DocsExtractor('path-to-nonexistent-file.js', {
-                tags: {
-                    startDocs: '<docs>',
-                    endDocs: '</docs>',
-                }
+                extractMethod: 'tags',
+                startTag: '<docs>',
+                endTag: '</docs>',
             });
 
-            const result = docsExtractor.extract();
+            const result = await docsExtractor.extract();
 
             expect(result.sucess).toBe(false);
             expect(result.errorType).toBe('fileNotFound');
         })
+    });
+
+    describe("extract using regex", () => {
+        test("should perform expected behavior", async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'regex',
+                pattern: new RegExp(/<docs>(.*?)<\/docs>/s),
+            });
+
+            const result = await docsExtractor.extract();
+
+            expect(result.sucess).toBe(true);
+            expect(result.docs).toContain('#exampleFunc.js');
+        });
+
+        test("should return an error if the file does not contain the regex", async () => {
+            docsExtractor = new DocsExtractor(fileWithoutDocs, {
+                extractMethod: 'regex',
+                pattern: new RegExp('/<docs>(.*?)<\/docs>/s'),
+            });
+
+            const result = await docsExtractor.extract();
+
+            expect(result.sucess).toBe(false);
+            expect(result.errorType).toBe('noDocs');
+        });
+    });
+
+    describe("extract using callback", () => {
+        test("should perform expected behavior", async () => {
+            docsExtractor = new DocsExtractor(fileWithDocs, {
+                extractMethod: 'callback',
+                callback: async (fileContent) => {
+                    return fileContent.match('/<docs>(.*?)<\/docs>/s')?.[1];
+                },
+            });
+        });
+
+        test("should return an error if the file does not contain the callback", async () => {
+            docsExtractor = new DocsExtractor(fileWithoutDocs, {
+                extractMethod: 'callback',
+                callback: async (fileContent) => {
+                    return undefined;
+                },
+            });
+
+            const result = await docsExtractor.extract();
+
+            expect(result.sucess).toBe(false);
+            expect(result.errorType).toBe('noDocs');
+        });
     });
 });
